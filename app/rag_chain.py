67dafd80e6cffe9093cf0fa0ee8b7f8141a39810
abdoul <abdoult15@live.fr>
2025-06-06 01:12:32 +0200

    cleaning

--- app/rag_chain.py
+++ app/rag_chain.py
@@ -1,42 +1,28 @@
-# from pymongo import MongoClient
-# from langchain.schema import Document
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_community.embeddings import OpenAIEmbeddings
-# from langchain_community.embeddings import HuggingFaceEmbeddings
-# from langchain_community.vectorstores import FAISS
-# from langchain_community.chat_models import ChatOpenAI
-# from langchain.chains import RetrievalQA,LLMChain
-# from langchain.prompts import PromptTemplate
-from langchain_community.llms import Ollama #HuggingFacePipeline
+
 from qdrant_client import QdrantClient
-from langchain.vectorstores import Qdrant
 import os
-# import tqdm
 from dotenv import load_dotenv
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline,AutoModelForSeq2SeqLM
 from sentence_transformers import SentenceTransformer
-
-load_dotenv()
-
-# ask_rag.py
-
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 from fastapi.responses import StreamingResponse
 import json
 import time
 
+load_dotenv()
+
 # === CONFIGURATION ===
-LLM_API_URL = "http://ollama:11434/api/generate"  # Nom du conteneur Docker
-COLLECTION_NAME = "rgpd_chunks"
-QDRANT_HOST = "qdrant"
-QDRANT_PORT = 6333
+LLM_API_URL =os.getenv("LLM_API_URL") 
+COLLECTION_NAME =os.getenv("QDRANT_COLLECTION_NAME")
+QDRANT_HOST =os.getenv("QDRANT_HOST") 
+QDRANT_PORT =os.getenv("QDRANT_PORT")
 
 # === INITIALISATION ===
 app = FastAPI()
 # Chargement du modèle d'embedding
 embedder = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+
 # Connexion à Qdrant
 qdrant = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
 
@@ -96,49 +82,4 @@ def ask_question(data: QuestionRequest):
     print(f"Retrieval time: {t1 - t0:.2f}s")
     print(f"LLM generation time: {t2 - t1:.2f}s")
 
-    #return {"answer": answer} #old version to avoid async_generator error
     return answer
-
-
-# def build_qa_chain():
-
-#     # Paramètres Qdrant
-#     QDRANT_HOST="localhost" #os.getenv("QDRANT_HOST", "localhost")
-#     QDRANT_PORT= 6333 #int(os.getenv("QDRANT_PORT", 6333))
-#     QDRANT_COLLECTION_NAME="rgpd_chunks"#os.getenv("QDRANT_COLLECTION_NAME", "rgpd_chunks")
-
-
-#     LLM_API_URL = "http://localhost:11434/api/generate" 
-
-
-#     # Connexion à la BDD vectorielle
-#     qdrant_client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
-    
-    
-#     # Vectorisation des textes
-#     # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-#     embeddings=SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
-#     # Connexion au vectorstore avec LangChain
-#     vectorstore = Qdrant(
-#         client=qdrant_client,
-#         collection_name=QDRANT_COLLECTION_NAME,
-#         embeddings=embeddings
-#     )
-#     retriever = vectorstore.as_retriever()
-#     # llm = ChatOpenAI(temperature=0)
-#     # llm_ollama = Ollama(model="mistral")
-#     llm_ollama = Ollama(model="llma3",base_url="http://ollama:11434")
-
-
-#     # Construction de la chaîne QA
-#     qa_chain = RetrievalQA.from_chain_type(
-#         llm=llm_ollama,
-#         retriever=retriever,
-#         # chain_type="stuff",  # important
-#         # chain_type_kwargs={"prompt": prompt_template}
-#     )
-
-    
-#     return qa_chain
-
-# RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -4,60 +4,12 @@ import os
 
 #Favicon
 st.set_page_config(page_title="Assistant Juridique", page_icon="⚖️")
-# API_URL = "http://rag-api:8000/ask"  # Modifie si tu déploies ailleurs
+
 API_URL = os.getenv("API_URL", "http://rag-api:8000/ask")  # fallback pour local
 
-st.title("💬 Chatbot RAG spécialisé")
-#===============Version chat gpt BEGIN=======================================
-# # 🟦 Initialisation de l'historique si absent
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
+st.title("💬 Chatbot RAG spécialisé en RGPD")
 
-# # 🟦 Affichage de l'historique
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
 
-# # 🟦 Saisie utilisateur
-# if prompt := st.chat_input("Pose une question sur le domaine juridique..."):
-#     if prompt.strip() == "":
-#         st.warning("Merci de poser une vraie question.")
-#         st.stop()  # ✅ Empêche appel API vide (corrige 422)
-        
-#     # Ajout message utilisateur
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-#     with st.chat_message("user"):
-#         st.markdown(prompt)
-
-#     # 🟦 Appel API
-#     with st.chat_message("ai"):
-#         with st.spinner("Recherche de réponse..."):
-#             try:
-#                 response = requests.post(API_URL, json={"question": prompt}, timeout=60)
-
-#                 if response.status_code == 200:
-#                     # ✅ Utilisation de st.empty() pour éviter les doublons
-#                     result = response.json()["answer"]
-#                     container = st.empty()
-#                     container.markdown(result)
-#                 else:
-#                     result = f"❌ Erreur API : {response.status_code}"
-#                     st.error(result)
-#             except requests.RequestException as e:
-#                 result = f"❌ Erreur API : {e}"
-#                 st.error(result)
-
-#         # ✅ Mise à jour historique après récupération
-#         st.session_state.messages.append({"role": "assistant", "content": result})
-
-# # 🟦 Réinitialisation conversation
-# if st.button("🗑️ Effacer la conversation"):
-#     st.session_state.messages = []
-#     st.rerun()
-
-#===============Version chat gpt END=======================================
-
-#===============Version Axel BEGIN=======================================
 # Historique des messages
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -67,64 +19,41 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# user input
-if prompt := st.chat_input("Pose une question sur le domaine juridique..."):
+# Entrée utilisateur
+if prompt := st.chat_input("Pose une question sur le RGPD"):
     if prompt.strip() == "":
         st.warning("Merci de poser une vraie question.")
-        st.stop()  # ✅ Empêche appel API vide (corrige 422)
+        st.stop()  # Empêche appel API vide (corrige 422)
         
     # Ajout message utilisateur
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-# if prompt := st.chat_input("Pose une question sur le RGPD..."):
-#     with st.chat_message("user"):
-#         # Gestion de l'erreur 442
-#         if prompt.strip() == "":
-#             st.warning("Merci de poser une vraie question.")
-#             st.stop()
-#         st.markdown(prompt)
-#     # MAJ de l'historique
-#     st.session_state.messages.append({"role": "user", "content": prompt})
 
     # RAG API
     with st.chat_message("ai"):
-        # question = prompt
         with st.spinner("Recherche de réponse..."):
             try : 
-                #=============old version BEGIN=========================================================
-                # response = requests.post(API_URL, json={"question": prompt}, timeout=180)
-                # if response.status_code == 200:
-                #     #res = response.json().get("answer")["result"] 
-                #     #result = f"Echo: {res}"
-                #     #result = response.json().get("answer" , {}).get("result", "❓ Aucune réponse trouvée.")
-                #     result = response.json()["answer"]
-                #     #st.markdown(result)
-                # else:
-                #     result = f"❌ Erreur API : {response.status_code}"
-                #=============old version END=========================================================
-                # ✅ Correction : on active le stream de la réponse
+                
+                # Activation du stream de la réponse
                 response = requests.post(API_URL, json={"question": prompt}, stream=True, timeout=180)
 
                 if response.status_code == 200:
                     response_box = st.empty() # conteneur temporaire pour éviter les réponses multiples
                     result = ""
-                    # ✅ Correction : lecture ligne par ligne du flux streamé
+                    # Lecture ligne par ligne du flux streamé
                     for line in response.iter_lines():
                         if line:
                             chunk = line.decode("utf-8")
                             result += chunk
-                            #break
                             #mise à jour dynamique du conteneur
                             response_box.markdown(result + "▌")
                     response_box.markdown(result)  # Affichage final sans le curseur
                 else:
-                    result = f"❌ Erreur API : {response.status_code}"
+                    result = f"Erreur API : {response.status_code}"
                     st.markdown(result)
             except requests.RequestException as e:
-                # st.error(f"Erreur lors de la récupération de la réponse:{response.status_code}")
-                # st.markdown(response)
-                result = f"❌ Erreur API : {e}"
+                result = f"Erreur API : {e}"
                 #Affichage reponse
                 st.error(result)
         # MAJ historique
@@ -133,61 +62,6 @@ if prompt := st.chat_input("Pose une question sur le domaine juridique..."):
 
 
 #Renitialisation conversation
-if st.button("🗑️ Effacer la conversation"):
+if st.button("Effacer la conversation"):
     st.session_state.messages = []
     st.rerun()
-#===============Version Axel END=======================================
-
-# import streamlit as st
-# import requests
-# import os
-
-# # API_URL = "http://rag-api:8000/ask"  # Modifie si tu déploies ailleurs
-# API_URL = os.getenv("API_URL", "http://rag-api:8000/ask")  # fallback pour local
-
-# # interface.py
-
-# st.title("Assistant RGPD 🤖")
-
-# question = st.text_input("Posez votre question sur le RGPD :")
-
-# if st.button("Envoyer") and question:
-#     with st.spinner("Réflexion en cours..."):
-#         response = requests.post(
-#             "http://rag-api:8000/ask",  # L'API FastAPI locale
-#             json={"question": question}
-#         )
-#         if response.status_code == 200:
-#             st.markdown("### 💬 Réponse")
-#             st.write(response.json()["answer"])
-#         else:
-#             st.error("Erreur côté serveur.")
-
-
-
-# # st.title("💬 Chatbot RAG spécialisé")
-# # st.write("Pose une question sur ton domaine...")
-
-# # question = st.text_input("Ta question :", placeholder="Ex: Quels sont les effets du changement climatique ?")
-
-# # if st.button("Envoyer") and question:
-# #     with st.spinner("Recherche de réponse..."):
-# #         response = requests.post(API_URL, json={"question": question})
-# #         if response.status_code == 200:
-# #             st.success(response.json().get("answer"))
-# #         else:
-# #             st.error(f"Erreur lors de la récupération de la réponse:{response.status_code}")
-
-# # # Zone de texte pour l'utilisateur
-# # user_input = st.text_input("Posez votre question:")
-# # # Affichage des réponses
-# # if user_input:
-# #     with st.spinner("Réflexion en cours..."):
-# #         response = requests.post(
-# #             "http://localhost:8000/ask",  # Ton endpoint FastAPI
-# #             json={"question": user_input}
-# #         )
-# #         if response.status_code == 200:
-# #             st.markdown("**Réponse :** " + response.json()["answer"])
-# #         else:
-# #             st.error("Erreur de l'API : " + response.text)
